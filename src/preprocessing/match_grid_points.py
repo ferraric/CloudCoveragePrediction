9c93ac1fd1e72c743824414751f37ce45ac328eb
Yinghao Dai <yidai@student.ethz.ch>
2019-10-08 11:05:33 +0200

    Create mapping dictionary from prediction x-y to label lon-lat

--- src/preprocessing/match_grid_points.py
+++ src/preprocessing/match_grid_points.py
@@ -1,19 +1,32 @@
 import xarray as xr
 import numpy as np
-import pickle
+import pickle, os
+
+def distance(x, y):
+    return np.linalg.norm(np.array(x) - np.array(y), 2)
+
+def write_pkl(object, filepath):
+    f = open(filepath, "wb")
+    pickle.dump(object, f)
+    f.close()
+
 
 # change the following two lines, depending on your machine
-labels = xr.open_mfdataset("play_data/meteosat.CFC.H_ch05.latitude_longitude_20180221*.nc")
-predictions = xr.open_mfdataset("play_data/cosmo-e_2018022100_CLCT.nc")
+labels = xr.open_mfdataset("../local_playground/meteosat.CFC.H_ch05.latitude_longitude_20180221*.nc")
+predictions = xr.open_mfdataset("../local_playground/cosmo-e_2018022100_CLCT.nc")
 
 lat = labels["lat"].values
 lon = labels["lon"].values
-lon_lat_grid = np.transpose([np.tile(lon, len(lat)), np.repeat(lat, len(lon))])
-lon_lat_grid = [tuple(map(lambda a: round(a, 3), y)) for y in lon_lat_grid]  # 5500 pairs
-prediction_coordinates = predictions[["lon_1", "lat_1"]].to_dataframe().values  # shape (23876, 2)
+lat_lon_grid = np.transpose([np.tile(lat, len(lon)), np.repeat(lon, len(lat))])  # 5500 pairs
+prediction_df = predictions[["lon_1", "lat_1"]].to_dataframe()
+prediction_coordinates = prediction_df.values[:, ::-1]  # shape (23876, 2)
 prediction_coordinates = np.unique(prediction_coordinates, axis=1)
-lookup_table = {tuple(x): lon_lat_grid[np.argmin([np.linalg.norm(x - c, 2) for c in lon_lat_grid])]
-                for x in prediction_coordinates}
-f = open("lookup_table.pkl", "wb")
-pickle.dump(lookup_table, f)
-f.close()
+prediction_xy = prediction_df.index.values
+
+data_path = os.path.join("..", "..", "shared_data")
+lookup_table_ll_ll = {tuple(x): lat_lon_grid[np.argmin([distance(x, c) for c in lat_lon_grid])]
+                      for x in prediction_coordinates}
+write_pkl(lookup_table_ll_ll, os.path.join(data_path, "lookup_ll_ll.pkl"))
+
+lookup_table_xy_ll = {tuple(x): lookup_table_ll_ll[tuple(prediction_df.loc[x, :])] for x in prediction_xy}
+write_pkl(lookup_table_xy_ll, os.path.join(data_path, "lookup_xy_ll.pkl"))
