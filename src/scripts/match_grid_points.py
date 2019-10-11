fa6e72c99eaeb2048748204f624e1cbc6dde50e2
Yinghao Dai <yidai@student.ethz.ch>
2019-10-11 16:14:28 +0200

    Switch back order of longitude and latitude in lookup dictionary

--- src/scripts/match_grid_points.py
+++ src/scripts/match_grid_points.py
@@ -15,16 +15,16 @@ def write_pkl(object, filepath):
 labels = xr.open_mfdataset("../local_playground/meteosat.CFC.H_ch05.latitude_longitude_20180221*.nc")
 predictions = xr.open_mfdataset("../local_playground/cosmo-e_2018022100_CLCT.nc")
 
-lat = labels["lat"].values
 lon = labels["lon"].values
-lat_lon_grid = np.transpose([np.tile(lat, len(lon)), np.repeat(lon, len(lat))])  # 5500 pairs
+lat = labels["lat"].values
+lon_lat_grid = np.transpose([np.tile(lon, len(lat)), np.repeat(lat, len(lon))])  # 5500 pairs
 prediction_df = predictions[["lon_1", "lat_1"]].to_dataframe()
-prediction_coordinates = prediction_df.values[:, ::-1]  # shape (23876, 2)
+prediction_coordinates = prediction_df.values  # shape (23876, 2)
 prediction_coordinates = np.unique(prediction_coordinates, axis=1)
 prediction_xy = prediction_df.index.values
 
 data_path = os.path.join("..", "..", "shared_data")
-lookup_table_ll_ll = {tuple(x): lat_lon_grid[np.argmin([distance(x, c) for c in lat_lon_grid])]
+lookup_table_ll_ll = {tuple(x): lon_lat_grid[np.argmin([distance(x, c) for c in lon_lat_grid])]
                       for x in prediction_coordinates}
 write_pkl(lookup_table_ll_ll, os.path.join(data_path, "lookup_ll_ll.pkl"))
 
