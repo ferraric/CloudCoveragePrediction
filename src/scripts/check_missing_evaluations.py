f3818141dc2b6116abe166aeccbe6d3d37244dd6
Yinghao Dai <yidai@spaceml1.inf.ethz.ch>
2019-10-13 19:28:56 +0200

    Update script for checking evaluations

--- src/scripts/check_missing_evaluations.py
+++ src/scripts/check_missing_evaluations.py
@@ -4,6 +4,13 @@ import os
 folder = os.path.join("~", "evaluation")
 dates = pd.date_range("20140101 00", "20181226 12", freq="12H")
 file_names = ["persistence_CRPS_" + date.strftime("%Y%m%d%H") + ".pkl" for date in dates]
+no_consecutive = 0
 for name in file_names:
-    if not os.path.exists(name):
+    try:     
+        pd.read_pickle(os.path.join(folder, name))
+        if no_consecutive > 0:
+            print(no_consecutive)
+            no_consecutive = 0
+    except FileNotFoundError:
         print(name)
+        no_consecutive += 1
