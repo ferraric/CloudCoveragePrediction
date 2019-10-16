007d3876a8e641e513a990c7284b1326133375bc
Claudio Ferrari <ferraric@student.ethz.ch>
2019-10-16 10:59:26 +0200

    python is not simply pass by reference bugfix

--- src/preprocessing/nwp_preprocessing.py
+++ src/preprocessing/nwp_preprocessing.py
@@ -2,7 +2,7 @@ import xarray as xr
 import numpy as np
 
 
-def transform_nwp_data(data: xr.core.dataset.Dataset):
+def transform_nwp_data(data: xr.core.dataset.Dataset) -> xr.core.dataset.Dataset:
     """
     Transforms nwp data.
     Warning: this function directly modifies the input data
@@ -11,11 +11,12 @@ def transform_nwp_data(data: xr.core.dataset.Dataset):
         data:     xarray dataset to be transformed
 
     """
-    __crop_to_fit_label_grid(data)
+    data = __crop_to_fit_label_grid(data)
     try:
-        __clip_clct_data(data.CLCT)
+        data.CLCT = __clip_clct_data(data.CLCT)
     except AttributeError:
         pass
+    return data
 
 
 def __clip_clct_data(clct_data: xr.core.dataarray.DataArray):
@@ -23,7 +24,7 @@ def __clip_clct_data(clct_data: xr.core.dataarray.DataArray):
     return clct_data
 
 
-def __crop_to_fit_label_grid(data: xr.core.dataset.DataArray):
+def __crop_to_fit_label_grid(data: xr.core.dataset.Dataset) -> xr.core.dataset.Dataset:
     """
     From scripts/nwp_crop_test.py we learn that for the nwp data the following needs to hold
     in order for it to be completely within the label grid:
@@ -33,4 +34,4 @@ def __crop_to_fit_label_grid(data: xr.core.dataset.DataArray):
     Args:
         data:     xarray dataset to be cropped
     """
-    data = data.sel(x_1=slice(-2.99, 0.65), y_1=slice(-1.39, 0.98))
+    return data.sel(x_1=slice(-2.99, 0.65), y_1=slice(-1.39, 0.98))
