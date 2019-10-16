bd7e441bee95e798bf262b34cdf9dc2e580d22a1
Claudio Ferrari <ferraric@student.ethz.ch>
2019-10-16 11:08:28 +0200

    Update get_quantiles.py

--- src/scripts/get_quantiles.py
+++ src/scripts/get_quantiles.py
@@ -1,16 +1,13 @@
 import numpy as np
 
-
-def return_quantiles(input_arr, max_quantiles):
-    percentiles = []
+def return_quantiles(input_arr: np.ndarray, max_quantiles: int) -> np.ndarray:
+    quantiles = []
     for i in range(1, max_quantiles + 1):
-        percentiles.append(((i - 0.5) / max_quantiles))
-    q = np.percentile(input_arr, percentiles, axis=0)
-    return np.array(q)
-
+        quantiles.append(((i - 0.5) / max_quantiles))
+    return np.quantile(input_arr, quantiles, axis=0)
 
 #example usecase
-input_arr = np.random.randn(419, 5550)
+input_arr = np.arange(100)
 max_quantiles = 21
 f = return_quantiles(input_arr, max_quantiles)
 print(f.shape)
