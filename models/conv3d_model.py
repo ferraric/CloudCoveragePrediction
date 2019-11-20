5d5c9001314d14c0ffc4e44f9c3ae903be9fce8d
Claudio Ferrari <ferraric@ethz.ch>
2019-11-20 23:40:00 +0100

    new model architecture

--- models/conv3d_model.py
+++ models/conv3d_model.py
@@ -11,17 +11,25 @@ class Conv3dModel(Model):
         self.build_model()
 
     def build_model(self):
-        self.conv1 = Conv3D(kernel_size = [3,3,3], filters=4,
+        self.conv1 = Conv3D(kernel_size = [3,3,3], filters=16,
                             #dilation_rate=[2, 2, 2],
                             strides=[2,2,2], padding="valid", activation="relu",
                             input_shape=(self.config.batch_size, 121, 59, 90, 2))
-        self.conv2 = Conv3D(kernel_size = [3,3,3], filters=8,
+        self.conv2 = Conv3D(kernel_size = [3,3,3], filters=32,
                             #dilation_rate=[2, 2, 2],
                             strides=[2,2,2], padding="valid", activation="relu")
-        self.deconv1 = Conv3DTranspose(kernel_size = [3,3,3], filters=4,
+        self.conv3 = Conv3D(kernel_size = [3,3,3], filters=64,
+                            #dilation_rate=[2, 2, 2],
+                            #strides=[2,2,2],
+                            padding="valid", activation="relu")
+        self.deconv3 = Conv3DTranspose(kernel_size = [3,3,3], filters=32,
+                                       #strides=[2,2,2],
+                                       padding="valid", activation="relu",
+                                       output_padding=[0,0,0])
+        self.deconv2 = Conv3DTranspose(kernel_size = [3,3,3], filters=16,
                                        strides=[2,2,2], padding="valid", activation="relu",
                                        output_padding=[1,0,1])
-        self.deconv2 = Conv3DTranspose(kernel_size=[3,3,3], filters=2,
+        self.deconv1 = Conv3DTranspose(kernel_size=[3,3,3], filters=2,
                                        strides=[2,2,2], padding="valid", activation="relu",
                                        output_padding=[0,0,1])
 
@@ -33,8 +41,12 @@ class Conv3dModel(Model):
         #self.crop = Cropping2D(cropping=((1, 1), (1, 0)))
 
     def call(self, x):
+        input = x
         x = self.conv1(x)
         x = self.conv2(x)
-        x = self.deconv1(x)
+        x = self.conv3(x)
+        x = self.deconv3(x)
         x = self.deconv2(x)
+        x = self.deconv1(x)
+        x = tf.keras.layers.average([x, input])
         return x
