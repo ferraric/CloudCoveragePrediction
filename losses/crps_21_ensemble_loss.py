f1d4882827a387bc5564761f48fcd1912107db63
rheasukthanker <20137590+rheasukthanker@users.noreply.github.com>
2021-07-31 10:14:10 +0530

    Add files via upload

--- losses/crps_21_ensemble_loss.py
+++ losses/crps_21_ensemble_loss.py
@@ -4,7 +4,10 @@ from tensorflow.keras.losses import Loss
 from properscoring import crps_ensemble
 from scipy.spatial.distance import pdist, squareform
 from scipy.stats import norm
+
 tf.keras.backend.set_floatx('float64')
+
+
 class Crps21EnsembleLoss(Loss):
     # TODO: type annotations
     def call(self, y_true, y_pred):
@@ -20,12 +23,15 @@ class Crps21EnsembleLoss(Loss):
         #y_true = y_true[:, 0]
         y_pred = tf.reshape(y_pred, [-1, 21])
         #y_pred=tf.clip_by_value(y_pred,0,100)
-        n = tf.dtypes.cast(tf.shape(y_pred)[0], tf.float64) 
-        d = tf.dtypes.cast(tf.shape(y_pred)[1], tf.float64)  
+        n = tf.dtypes.cast(tf.shape(y_pred)[0], tf.float64)
+        d = tf.dtypes.cast(tf.shape(y_pred)[1], tf.float64)
         y_true = tf.reshape(y_true, [n, 1])
-        diffs = tf.math.reduce_sum(tf.math.abs(tf.math.subtract(y_pred, tf.broadcast_to(y_true, (n, d)))), axis=1)
-        pair_diff_sum = tf.map_fn(lambda row: tf.reduce_sum(tf.abs(tf.subtract(row, tf.expand_dims(row, 1)))), y_pred)
-        crps =  diffs / d - pair_diff_sum / (2 * d ** 2)
+        diffs = tf.math.reduce_sum(tf.math.abs(
+            tf.math.subtract(y_pred, tf.broadcast_to(y_true, (n, d)))),
+                                   axis=1)
+        pair_diff_sum = tf.map_fn(
+            lambda row: tf.reduce_sum(
+                tf.abs(tf.subtract(row, tf.expand_dims(row, 1)))), y_pred)
+        crps = diffs / d - pair_diff_sum / (2 * d**2)
 
         return tf.math.reduce_mean(crps)
-
--- losses/crps_ensemble_loss.py
+++ losses/crps_ensemble_loss.py
@@ -5,6 +5,7 @@ from tensorflow.keras.losses import Loss
 from properscoring import crps_ensemble
 from scipy.stats import norm
 
+
 class CrpsEnsembleLoss(Loss):
     #EPS = 0.0001
     # TODO: type annotations
@@ -20,27 +21,32 @@ class CrpsEnsembleLoss(Loss):
                 mean_crps: Scalar with mean CRPS over batch
             """
         # TODO: refactor this
-        y_true = tf.reshape(y_true,[tf.shape(y_true)[0]])
+        y_true = tf.reshape(y_true, [tf.shape(y_true)[0]])
         #y_pred = tf.reshape(y_pred, [-1, 2])
         #print(tf.shape(y_pred))
 
-        mu = tf.reshape(a[0],[tf.shape(a[0])[0]])
+        mu = tf.reshape(a[0], [tf.shape(a[0])[0]])
         mu = tf.clip_by_value(mu, clip_value_min=0, clip_value_max=100)
-        var = tf.reshape(a[1],[tf.shape(a[1])[0]])
+        var = tf.reshape(a[1], [tf.shape(a[1])[0]])
         # since model might predict negative var
         stdev = tf.math.sqrt(tf.math.abs(var))
         #var = tf.clip_by_value(var, clip_value_min=self.EPS, clip_value_max=np.inf)
-        stdev = tf.clip_by_value(stdev, clip_value_min=0.0001, clip_value_max=np.inf)
+        stdev = tf.clip_by_value(stdev,
+                                 clip_value_min=0.0001,
+                                 clip_value_max=np.inf)
         no_quantiles = 21
-        quantiles = [(i - 0.5) / no_quantiles for i in range(1, no_quantiles + 1)]
+        quantiles = [(i - 0.5) / no_quantiles
+                     for i in range(1, no_quantiles + 1)]
         quantiles = np.broadcast_to(quantiles, (mu.shape[0], no_quantiles))
 
-        y_ensemble_pred = np.transpose(norm.ppf(np.transpose(quantiles), loc=mu, scale=stdev))
-        y_ensemble_pred = tf.clip_by_value(y_ensemble_pred, clip_value_min=0, clip_value_max=100)
+        y_ensemble_pred = np.transpose(
+            norm.ppf(np.transpose(quantiles), loc=mu, scale=stdev))
+        y_ensemble_pred = tf.clip_by_value(y_ensemble_pred,
+                                           clip_value_min=0,
+                                           clip_value_max=100)
 
         #y_ensemble_pred = tfp.distributions.MultivariateNormalDiag(loc=mu, scale_diag=var) \
         #    .quantile(tf.convert_to_tensor([(i - 0.5) / no_quantiles for i in range(1, no_quantiles + 1)]))
 
         crps = crps_ensemble(y_true, y_ensemble_pred)
         return tf.math.reduce_mean(crps)
-
