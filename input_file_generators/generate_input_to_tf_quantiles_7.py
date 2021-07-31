046e0ae23bac78276ad199223739cc0d812b0056
rheasukthanker <20137590+rheasukthanker@users.noreply.github.com>
2021-07-31 10:13:08 +0530

    Add files via upload

--- input_file_generators/generate_input_to_tf_quantiles_7.py
+++ input_file_generators/generate_input_to_tf_quantiles_7.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+
 column = "labels"
-filepath = "/mnt/ds3lab-scratch/yidai/nwp_subsampled_2x2_5_day_stride_ensemble_7quantiles/2018_with_" + column 
+filepath = "/mnt/ds3lab-scratch/yidai/nwp_subsampled_2x2_5_day_stride_ensemble_7quantiles/2018_with_" + column
 init_time_00 = pd.read_pickle(filepath + "_it00.pkl")
 init_time_12 = pd.read_pickle(filepath + "_it12.pkl")
 # pick an arbitrary date to get dictionary of all possible lat lon pairs
@@ -30,23 +31,27 @@ new_df_00["CLCT_quantile6"] = init_time_00["CLCT_quantile6"]
 new_df_12["CLCT_quantile6"] = init_time_12["CLCT_quantile6"]
 new_df_00["labels"] = init_time_00["labels"]
 new_df_12["labels"] = init_time_12["labels"]
-new_df_00["lat_lon_id"] = [dict[(init_time_00["lat_1"][i], init_time_00["lon_1"][i])] for i in range(0, init_time_00.shape[0])]
-new_df_12["lat_lon_id"] = [dict[(init_time_12["lat_1"][i], init_time_12["lon_1"][i])] for i in range(0, init_time_12.shape[0])]
+new_df_00["lat_lon_id"] = [
+    dict[(init_time_00["lat_1"][i], init_time_00["lon_1"][i])]
+    for i in range(0, init_time_00.shape[0])
+]
+new_df_12["lat_lon_id"] = [
+    dict[(init_time_12["lat_1"][i], init_time_12["lon_1"][i])]
+    for i in range(0, init_time_12.shape[0])
+]
 new_df_00["hour"] = [x.hour for x in init_time_00["time"]]
-new_df_00["init_time_day"]=[x.day for x in init_time_00["init_time"]]
+new_df_00["init_time_day"] = [x.day for x in init_time_00["init_time"]]
 new_df_00["year"] = [x.year for x in init_time_00["time"]]
 new_df_00["month"] = [x.month for x in init_time_00["time"]]
 new_df_00["lead_time"] = init_time_00["lead_time"]
-new_df_00["init_time"] = [0 for i in range(0,init_time_00.shape[0])]
+new_df_00["init_time"] = [0 for i in range(0, init_time_00.shape[0])]
 new_df_12["hour"] = [x.hour for x in init_time_12["time"]]
-new_df_12["init_time_day"]=[x.day for x in init_time_12["init_time"]]
+new_df_12["init_time_day"] = [x.day for x in init_time_12["init_time"]]
 new_df_12["year"] = [x.year for x in init_time_12["time"]]
 new_df_12["month"] = [x.month for x in init_time_12["time"]]
 new_df_12["lead_time"] = init_time_12["lead_time"]
-new_df_12["init_time"] = [1 for i in range(0,init_time_12.shape[0])]
+new_df_12["init_time"] = [1 for i in range(0, init_time_12.shape[0])]
 new_df_00 = new_df_00[np.isfinite(new_df_00["labels"])]
 new_df_12 = new_df_12[np.isfinite(new_df_12["labels"])]
-full_data = pd.concat([new_df_00,new_df_12],axis=0)
-full_data.to_pickle("2018_test_set" +column + ".pkl")
-
-
+full_data = pd.concat([new_df_00, new_df_12], axis=0)
+full_data.to_pickle("2018_test_set" + column + ".pkl")
